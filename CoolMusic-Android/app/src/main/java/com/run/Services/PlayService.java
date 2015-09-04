eadebcc2deaac082fdaa93c593a4234e2db1d8c2
qchw <qingchun_w@sina.com>
2015-09-04 11:07:11 +0800

    修正点击暂停之后再点击播放按钮无响应的BUG以及相应的显示问题
    1. 修正点击暂停之后再点击播放按钮无响应的BUG：PlayService.java中的pause函数出现了问题，点击暂停之后没有将isPlaying正确重置，导致LocalMusicActivity.java中播放按钮的监听器总是探知正在播放
    2. 添加Android Studio自动生成的文件

--- CoolMusic-Android/app/src/main/java/com/run/Services/PlayService.java
+++ CoolMusic-Android/app/src/main/java/com/run/Services/PlayService.java
@@ -9,7 +9,8 @@ import java.io.IOException;
  */
 public class PlayService {
 
-    private boolean isPlaying = false;
+    private boolean isPlaying = false;//是否正在播放
+    private boolean hasPaused = false;//是否正在暂停播放
     MediaPlayer player = null;
 
     public PlayService()
@@ -17,11 +18,16 @@ public class PlayService {
         player = new MediaPlayer();
     }
 
+    /*
+    * 播放音乐
+    *
+    * @param filePathString 播放音乐文件地址
+    * */
     public void play(String filePathString) throws IOException {
         if(player == null) {
             player = new MediaPlayer();
         }
-        if(isPlaying)
+        if(isPlaying || hasPaused)
         {
             //正在播放中的时候，需要将MediaPlayer重置，否则播放新音乐的时候回出现错误
             player.stop();
@@ -38,11 +44,16 @@ public class PlayService {
     public void stop() {
         player.stop();
         isPlaying = false;
+        hasPaused = false;
     }
 
     public void pause()
     {
         player.pause();
+        //播放状态切换成暂停
+        isPlaying = false;
+        //是否暂停过设置成true
+        hasPaused = true;
     }
 
     public void reStart(){
@@ -52,6 +63,7 @@ public class PlayService {
             //player被释放之后的处置措施
             //重建，并且应当保存上一次播放的歌曲信息以及播放时长
         isPlaying = true;
+        hasPaused = false;
     }
 
     public boolean isPlaying()
